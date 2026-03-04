5cff300d0b6f0b973f645c192f7b0dec8ebd98a7
Fotis Giasemis <fotis@fotisgiasemis.com>
2026-03-04 19:39:58 +0200

    Add returns prediction changes

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ cd stgan/STGAN
 mamba run -n stgan \
     python main.py \
     --dataset crypto_volume \
-    --lambda_G 10 \
+    --lambda_G 1 \
     --trend_time 7 \
     --epoch 20 \
     2>&1 | tee crypto_volume/result/training.log
